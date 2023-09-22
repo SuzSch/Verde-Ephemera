5e9caa58ef101443a177fa433c805a0786501a05
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-22 11:04:20 -0700

    Continue working on the header as a reusable component for app.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
 
 function Header() {
+  
+  const handleDropdownClick = () => {
+    
+  };
+
+  const handleButtonClick = () => {
+    
+  };
+
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">
-          <img src="/path/to/your/logo.png" alt="Logo" />
-        </Link>
+        <a href="/">Your Logo</a>
       </div>
-      <nav className="nav">
-        <ul>
-          <li>
-            <Link to="/page1">Page 1</Link>
-          </li>
-          <li>
-            <Link to="/page2">Page 2</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="dropdown-menu" onClick={handleDropdownClick}>
+        {/* Dropdown content */}
+      </div>
+      <div className="button" onClick={handleButtonClick}>
+        {/* Button content */}
+      </div>
     </header>
   );
 }
