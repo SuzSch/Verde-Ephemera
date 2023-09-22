829a0a5cf7b45e1b0eb0b533cc209d8f7582573d
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-22 14:07:14 -0700

    WIP Home component and css.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import Header from './Header';
+import Header from './Header'; 
+import './Home.css'; 
 
 function Home() {
   return (
     <div>
       <Header />
-      <h1>Home Page Things and Stuff</h1>
+      <div className="main-box">
+        <h1>Welcome to VerdeEphemera</h1>
+        <h3>(Savor Life's Temporal Joys)</h3>
+        <div className="picture-gallery">
+          <img src="image1.jpg" alt="Image 1" />
+          <img src="image2.jpg" alt="Image 2" />
+          <img src="image3.jpg" alt="Image 3" />
+        </div>
+      </div>
+      <div className="shop-info">
+        <div className="shop-photo">
+          <img src="../images/home-page-pic.jpg" alt="Shop Photo" />
+        </div>
+        <div className="shop-description">
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            vitae elit libero, a pharetra augue. Sed posuere consectetur est at
+            lobortis. Vestibulum id ligula porta felis euismod semper.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
