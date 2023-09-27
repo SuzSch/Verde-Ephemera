af7c154a8c606cb3c51a0e25f6abc196d5e435da
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-27 13:28:09 -0700

    Add package for carousel, update slider component, add place holder images.

--- src/Components/Florist/FloristControl.js
+++ src/Components/Florist/FloristControl.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import Slider from "../Slider";
+
 
 
 function FloristControl() {
 
+  const sliderImages = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+  ];
   return (
 
     <div>
       <Header />
       <h1>This is the Florist Page</h1>
+      <Slider images={sliderImages} interval={5000} autoplay={true}/>
       <Footer />
     </div>
   );
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-function Slider() {
-
+function Slider({ images, interval, autoplay }) {
   return (
-
-
+    <Carousel interval={interval} autoPlay={autoplay}>
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Slide ${index}`} />
+        </div>
+      ))}
+    </Carousel>
   );
 }
 
