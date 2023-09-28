27d5bac4f98b1f00e8e44161f7bf792037ada903
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-28 14:57:41 -0700

    WIP cart component research.

---
+++ src/Components/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Cart({ items, onRemoveItem, onCheckout }) {
+  // Implement the shopping cart UI and functionality here
+  // You can map through the 'items' array to display each item in the cart
+  // Add buttons or actions to remove items and proceed to checkout
+  return (
+    <div className="cart">
+      
+      {items.map((item, index) => (
+        <div key={index} className="cart-item">
+          
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+          <button onClick={() => onRemoveItem(index)}>Remove</button>
+        </div>
+      ))}
+      <p>Total: ${items.reduce((total, item) => total + item.price, 0)}</p>
+      <button onClick={onCheckout}>Checkout</button>
+    </div>
+  );
+}
+
+export default Cart;
