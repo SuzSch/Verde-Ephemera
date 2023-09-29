0056a55b535d38c6ffffdcf27717d129a42a987b
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-29 14:57:30 -0700

    WIP item css.

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -8,19 +8,17 @@ function Item({ title, description, price, image, buttonText, onButtonClick }) {
 
 
     <div class="card">
-      <img src={image} alt={title} />
-  <div class="overlay"></div>
-  <button className="card-btn" onClick={onButtonClick}>{buttonText}</button>
-</div>
+      <div class="card-details">
+        <img src={image} alt={title} class="card-image" />
+        <h2>{title}</h2>
+        <h3>{description}</h3>
+        <h3>{price}</h3>
+      </div>
+      <button className="card-button" onClick={onButtonClick}>
+        {buttonText}
+      </button>
+    </div>
 
-    // <div className="item">
-    //   <img src={image} alt={title} />
-    //   <h3>{title}</h3>
-    //   <p>{description}</p>
-    //   <p>{price}</p>
-    //   <button className="item-button" onClick={onButtonClick}>{buttonText}</button>
-    // </div>
-   
   );
 }
 
