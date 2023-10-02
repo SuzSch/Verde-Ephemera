97f51480e51399d7ec9f2cd62ebe7237dbf3a300
Suzanne Schuber <suzanneschuber@gmail.com>
2023-10-02 10:47:18 -0700

    Add firbase config.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+
+const firebaseApp = initializeApp({
+
+  apiKey: "AIzaSyB1iQOQlhcfFzuRYUcWR1Gjhv8SdvACtXs",
+  authDomain: "verdeephemera.firebaseapp.com",
+  projectId: "verdeephemera",
+  storageBucket: "verdeephemera.appspot.com",
+  messagingSenderId: "872360875942",
+  appId: "1:872360875942:web:45f582c6588199ed3ea167",
+  measurementId: "G-38LNS1N94J"
+});
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
