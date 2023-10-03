6152f4c8db35fa0a777251639727872b9954281d
Suzanne Schuber <suzanneschuber@gmail.com>
2023-10-02 17:13:53 -0700

    Update text fade-in for home page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Header from './Header'; 
+import React, { useEffect, useRef } from 'react';
+import Header from './Header';
 import Footer from './Footer';
-import './Home.css'; 
+import './Home.css';
 import shopPhoto from '../images/home-page-pic.jpg';
 import bakeryHomePhoto from '../images/bakery-home.jpg';
 import floristHomePhoto from '../images/florist-home.jpg';
@@ -9,17 +9,41 @@ import libraryHomePhoto from '../images/library-home.jpg';
 import cafeHomePhoto from '../images/cafe-home.jpg';
 
 function Home() {
+  const mainBoxRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            mainBoxRef.current.classList.add('in-view');
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+    observer.observe(mainBoxRef.current);
+
+    return () => {
+      observer.unobserve(mainBoxRef.current);
+    }
+  }, []);
+
   return (
     <div>
       <Header />
-      <div className="main-box">
-        <h1>Welcome to VerdeEphemera</h1>
-        <h3>(Savor Life's Temporal Joys)</h3>
+      <div className="main-box" ref={mainBoxRef}>
         <div className="picture-gallery">
-          <img src={bakeryHomePhoto} alt="Image 1" />
-          <img src={floristHomePhoto} alt="Image 2" />
-          <img src={libraryHomePhoto} alt="Image 3" />
-          <img src={cafeHomePhoto} alt="Image 3" />
+          <div className='text-overlay'>
+            <h1>Welcome to VerdeEphemera</h1>
+            <h3>(Savor Life's Temporal Joys)</h3>
+          </div>
+            <img src={bakeryHomePhoto} alt="Image 1" />
+            <img src={floristHomePhoto} alt="Image 2" />
+            <img src={libraryHomePhoto} alt="Image 3" />
+            <img src={cafeHomePhoto} alt="Image 3" />
         </div>
       </div>
       <div className="shop-info">
