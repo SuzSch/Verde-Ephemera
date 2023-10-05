9f113b31b1c17edd56346af3a90884f206660223
Suzanne Schuber <suzanneschuber@gmail.com>
2023-10-05 11:52:12 -0700

    add total to shopping cart component.

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -2,6 +2,17 @@ import React from "react";
 import './Cart.css';
 
 function Cart({ cart, removeFromCart, floristItems }) {
+
+  function calculateTotalPrice(cart, floristItems) {
+    let totalPrice = 0;
+    for (const itemIndex of cart) {
+      totalPrice += parseFloat(floristItems[itemIndex].price.replace('$', ''));
+    }
+
+    return totalPrice.toFixed(2);
+  }
+
+  const totalPrice = calculateTotalPrice(cart, floristItems);
   return (
     <div className="cart">
       <h2>Shopping Cart</h2>
@@ -9,11 +20,14 @@ function Cart({ cart, removeFromCart, floristItems }) {
         {cart.map((itemIndex, index) => (
           <li key={index}>
             <span>{floristItems[itemIndex].description} - {floristItems[itemIndex].price}</span>
-            <span> <img src={floristItems[itemIndex].image}/></span>
+            <span> <img src={floristItems[itemIndex].image} /></span>
             <button onClick={() => removeFromCart(index)}>Remove</button>
           </li>
         ))}
       </ul>
+      <div className="total">
+        <strong>Total Price:</strong> ${totalPrice}
+      </div>
     </div>
   );
 }
